6b13b169904f6205adf1ff991aaf957387d0ea48
Hank <hsudo.me@protonmail.com>
2026-07-22 03:44:41 +0800

    move to vscodium

--- modules/user/IDE/IDE.nix
+++ modules/user/IDE/IDE.nix
@@ -6,9 +6,11 @@
         continue_config = config.lib.file.mkOutOfStoreSymlink "${IDE_prefix}/continue/continue.yml";
         template = config.lib.file.mkOutOfStoreSymlink "${IDE_prefix}/template/template.txt";
     in {
-        programs.vscode = {
+        programs.vscodium = {
             enable = true;
+            package = pkgs.vscodium;
             profiles.default.extensions = with pkgs.vscode-extensions; [
+                ms-ceintl.vscode-language-pack-zh-hant
                 ms-vscode-remote.remote-ssh
                 christian-kohler.path-intellisense
                 hediet.vscode-drawio
@@ -21,13 +23,16 @@
 
                 # nix
                 bbenoist.nix
-                
+
+                # c/c++
+                ms-vscode.cpptools-extension-pack
+
                 # python
                 ms-python.python
                 ms-toolsai.jupyter
             ];
         };
         home.file.".continue/config.yaml".source = continue_config;
-        home.file."Library/Application Support/Code/User/snippets/template.code-snippets".source = template;
+        home.file."Library/Application Support/VSCodium/User/snippets/template.code-snippets".source = template;
     };
 }
