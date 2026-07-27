5e27d43cf201952e0797981a2f54c86ea864ca2a
Hank <hsudo.me@protonmail.com>
2026-07-27 14:38:11 +0800

    vscodium python extension

--- modules/user/IDE/IDE.nix
+++ modules/user/IDE/IDE.nix
@@ -2,10 +2,41 @@
 
 {
     flake.modules.homeManager.IDE = { config, pkgs, ... }: let 
+        official_extension = with pkgs.vscode-extensions; [
+            ms-ceintl.vscode-language-pack-zh-hant
+            # ms-vscode-remote.remote-ssh
+            christian-kohler.path-intellisense
+            mkhl.direnv
+            hediet.vscode-drawio
+            pkief.material-icon-theme
+            continue.continue
+
+            # markdown
+            yzhang.markdown-all-in-one
+            shd101wyy.markdown-preview-enhanced
+
+            # nix
+            bbenoist.nix
+
+            # c/c++
+            ms-vscode.cpptools-extension-pack
+            llvm-vs-code-extensions.vscode-clangd
+
+            # python
+            ms-python.python
+            ms-toolsai.jupyter
+        ];
+        thirdparty_extension = with pkgs; [
+            open-vsx.jeanp413.open-remote-ssh
+            open-vsx.detachhead.basedpyright
+        ];
+
         IDE_prefix = "${config.home.homeDirectory}/sysconfig/modules/user/IDE";
+
         continue_config = config.lib.file.mkOutOfStoreSymlink "${IDE_prefix}/continue/continue.yml";
         user = config.lib.file.mkOutOfStoreSymlink "${IDE_prefix}/settings/user.json";
         template = config.lib.file.mkOutOfStoreSymlink "${IDE_prefix}/settings/template.txt";
+
         vscodiumUserDir = if pkgs.stdenv.isDarwin then "Library/Application Support/VSCodium/User" else ".config/VSCodium/User";
     in {
         nixpkgs.overlays = [
@@ -15,32 +46,7 @@
         programs.vscodium = {
             enable = true;
             package = pkgs.vscodium;
-            profiles.default.extensions = (with pkgs.vscode-extensions; [
-                ms-ceintl.vscode-language-pack-zh-hant
-                # ms-vscode-remote.remote-ssh
-                christian-kohler.path-intellisense
-                mkhl.direnv
-                hediet.vscode-drawio
-                pkief.material-icon-theme
-                continue.continue
-
-                # markdown
-                yzhang.markdown-all-in-one
-                shd101wyy.markdown-preview-enhanced
-
-                # nix
-                bbenoist.nix
-
-                # c/c++
-                ms-vscode.cpptools-extension-pack
-                llvm-vs-code-extensions.vscode-clangd
-
-                # python
-                ms-python.python
-                ms-toolsai.jupyter
-            ]) ++ [
-                pkgs.open-vsx.jeanp413.open-remote-ssh
-            ];
+            profiles.default.extensions = official_extension ++ thirdparty_extension;
         };
         home.file.".continue/config.yaml".source = continue_config;
         home.file."${vscodiumUserDir}/settings.json".source = user;
