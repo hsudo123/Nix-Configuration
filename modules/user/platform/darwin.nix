f810680a83ea7fdc6dbb23537d5917f70cbc4c38
Hank <hsudo.me@protonmail.com>
2026-08-09 21:33:19 +0800

    double pinyin

--- modules/user/platform/darwin.nix
+++ modules/user/platform/darwin.nix
@@ -1,7 +1,9 @@
 { inputs, ... }:
 
 {
-    flake.modules.homeManager.darwin = {
+    flake.modules.homeManager.darwin = { pkgs, config, ... }: let
+        grammar = config.lib.file.mkOutOfStoreSymlink "${config.home.homeDirectory}/sysconfig/modules/user/platform/double_pinyin/zh-hant-t-essay-bgc.gram";
+    in {
         imports = [
             inputs.mac-app-util.homeManagerModules.default
         ];
@@ -10,11 +12,9 @@
         home.stateVersion = "26.05";
         programs.home-manager.enable = true;
 
-        # 🎯 設定 1：主設定檔，將小鶴雙拼設為唯一/預設方案
         home.file."Library/Rime/default.custom.yaml".source = ./double_pinyin/default.custom.yaml;
-        # 🎯 設定 2：小鶴雙拼的個性化客製檔（實現強制繁體中文輸出 🇹🇼）
         home.file."Library/Rime/double_pinyin_flypy.custom.yaml".source = ./double_pinyin/double_pinyin_flypy.custom.yaml;
-        # 🎯 設定 3：透過 Nix 宣告式下載官方的小鶴雙拼 Schema 方案原始碼
+        home.file."Library/Rime/zh-hans-t-essay-bgc.gram".source = grammar;
         home.file."Library/Rime/double_pinyin_flypy.schema.yaml".source = ./double_pinyin/double_pinyin_flypy.schema.yaml;
     };
 }
